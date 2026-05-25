5811809aeeb9f6fca3d1584eb32b454a60c248e8
Vladislav <vf54640@gmail.com>
2026-05-25 21:44:00 +0300

    Add elite enemy and coin reward

--- src/app/classes.py
+++ src/app/classes.py
@@ -2,15 +2,32 @@
 
 class Enemy:
     """Defines the base attributes and behavior of a standard enemy."""
-    def __init__(self, hp: int):
+    def __init__(self, hp: int, reward: int):
         """Create an enemy instance with a specified amount of health."""
         self.hp = hp
+        self.reward = reward
 
-    def take_damage(self, damage: int):
+    def take_damage(self, damage: float) -> None:
         """Handle damage calculation and reduce enemy health."""
-        self.hp = max(0, self.hp - damage)
+        self.hp = round(max(0, self.hp - damage), 1)
 
     @property
     def is_alive(self) -> bool:
         """Check if the enemy is alive."""
         return self.hp > 0
+
+
+class EliteEnemy (Enemy):
+    """Defines attributes and behavior of an elite enemy."""
+    def __init__(self, hp: int, armor: float, reward: int):
+        """Create an elite enemy instance"""
+        super().__init__(hp, reward)
+        self.armor = armor
+
+    def take_damage(self, damage: float):
+        """Calculate damage reduction then applying."""
+        damage_block = round(damage * self.armor, 1)
+        final_damage = max(0, damage - damage_block)
+
+        print(f"Elite monster block {damage_block} damage.")
+        super().take_damage(final_damage)
--- src/app/main.py
+++ src/app/main.py
@@ -1,17 +1,22 @@
 """Starting mini-game here."""
 
-from classes import Enemy
+from random import randint
+import classes
 
-monster = Enemy(100)
+monster = classes.EliteEnemy(100, 0.2, randint(10, 25))
 
-attack = None
+coins = 0
+
 while True:
     print(f"Monster has {monster.hp} HP.")
 
-    attack = int(input("Enter damage for enemy: "))
+    attack = float(input("Enter damage for enemy: "))
     monster.take_damage(attack)
 
     if not monster.is_alive:
         print(f"Monster has {monster.hp} HP.")
         print("Monster has been defeated")
+        print(f"Monster dropped {monster.reward} coins.")
+        coins += monster.reward
+        print(f"You have {coins} coins.")
         break
