54918dc3e7179690ba0d68393071a86149e9e3ab
Vladislav <vf54640@gmail.com>
2026-05-26 20:50:11 +0300

    Add new battle system

--- src/app/main.py
+++ src/app/main.py
@@ -1,27 +1,44 @@
 """Starting mini-game here."""
 
-from random import randint
+from random import randint as ri
+
 import classes
 
-monster = classes.EliteEnemy(100, 0.2, randint(10, 25))
+
+def calculate_correct_answer(f_op: int, s_op: int) -> int:
+    """Return correct answer."""
+    return f_op + s_op
+
+
+def generate_math_problem() -> int:
+    """Generate all numbers, print the question and return correct answer."""
+    f_op = ri(0, 100)
+    s_op = ri(0, 100)
+    print("Solve this problem to dealt damage:")
+    print(f"{f_op} + {s_op} = ?")
+    return calculate_correct_answer(f_op, s_op)
+
+
+monster = classes.Enemy(100, 10)
 
 coins = 0
-
 while True:
-    print(f"Monster has {monster.hp} HP.")
-
+    print(f'Monster has {monster.hp} HP.')
+    correct_answer = generate_math_problem()
     try:
-        attack = float(input("Enter damage for enemy: "))
+        user_answer = int(input("Enter your answer: "))
     except ValueError:
-        print("Error, invalid input.")
+        print("Error, invalid input")
         continue
-    print("")
-    monster.take_damage(attack)
 
-    if not monster.is_alive:
-        print(f"Monster has {monster.hp} HP.")
-        print("Monster has been defeated")
-        print(f"Monster dropped {monster.reward} coins.")
-        coins += monster.reward
-        print(f"You have {coins} coins.")
-        break
+    if user_answer == correct_answer:
+        monster.take_damage(25)
+        if not monster.is_alive:
+            print(f"Monster has {monster.hp} HP.")
+            print("Monster has been defeated")
+            print(f"Monster dropped {monster.reward} coins.")
+            coins += monster.reward
+            print(f"You have {coins} coins.")
+            break
+    else:
+        print(f"Uncorrect answer, must be {correct_answer}.")
